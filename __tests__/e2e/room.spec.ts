c046ebb7300d2ac21e3ff296fd0c91ef86bba4e2
Luis Felipe Goulart <luisfelipegoulart94@gmail.com>
2025-09-23 15:34:00 -0300

    test(room): adjusts tests to responses

--- __tests__/e2e/room.spec.ts
+++ __tests__/e2e/room.spec.ts
@@ -20,20 +20,34 @@ describe("E2E -> Server", () => {
 
     expect(response.statusCode).toBe(201);
     expect(response.json()).toEqual({
-      ...defaultRoom,
-      id: expect.any(String),
-      code: expect.any(String),
-      createdAt: expect.any(String),
-      updatedAt: expect.any(String),
+      message: "Created",
+      count: 1,
+      total: 1,
+      data: {
+        ...defaultRoom,
+        id: expect.any(String),
+        code: expect.any(String),
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+      },
     });
   });
 
   it("should return a rooms list", async () => {
+    await app.inject({
+      method: "POST",
+      url: "/rooms",
+      payload: defaultRoom,
+    });
+
     const response = await app.inject({ method: "GET", url: "/rooms" });
 
     expect(response.statusCode).toBe(200);
     expect(response.json()).toEqual({
-      results: expect.arrayContaining([
+      message: "OK",
+      count: 1,
+      total: 1,
+      data: expect.arrayContaining([
         {
           id: expect.any(String),
           name: expect.any(String),
