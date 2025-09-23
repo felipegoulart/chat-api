029befa0c56d3dddcac34e87d03ffab3ad503dfa
Luis Felipe Goulart <luisfelipegoulart94@gmail.com>
2025-09-23 15:06:53 -0300

    test(room): add hook to clean all registry after each test

--- __tests__/e2e/helpers/setup.ts
+++ __tests__/e2e/helpers/setup.ts
@@ -1,6 +1,7 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import { connect } from "mongoose";
-import { afterAll, beforeAll } from "vitest";
+import { afterAll, afterEach, beforeAll } from "vitest";
+import { Room } from "../../../src/modules/room/model";
 
 const mongod = await MongoMemoryServer.create();
 
@@ -10,6 +11,10 @@ beforeAll(() => {
   console.log("Up the database... 🚀");
   connect(uri);
   console.log("Database is up! ✨");
+});
+
+afterEach(async () => {
+  await Room.deleteMany({});
 });
 
 afterAll(() => {
