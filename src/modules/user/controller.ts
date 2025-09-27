5425db392b2b1929e78147b55ea1d344ad256b47
Luis Felipe Goulart <luisfelipegoulart94@gmail.com>
2025-09-27 13:52:06 -0300

    feat(user): update password confirmation logic and improve validation messages

--- src/modules/user/controller.ts
+++ src/modules/user/controller.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcryptjs";
 import type { FastifyReply, FastifyRequest } from "fastify";
 import status from "http-status";
 import z from "zod";
@@ -7,29 +8,46 @@ export const createUserBodySchema = z
   .object({
     username: z.string().min(3).max(36),
     email: z.email(),
-    password: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/),
-    confirmPassword: z.string(),
+    password: z
+      .string()
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        "Your password must have at least 8 characters, one uppercase, one lowercase, one number and one special character",
+      ),
+    confirm: z.string(),
   })
-  .refine((data) => data.confirmPassword === data.password);
+  .refine((data) => data.confirm === data.password, {
+    error: "Passwords don't match",
+    path: ["confirm"],
+  });
 
 export type createUser = z.infer<typeof createUserBodySchema>;
 
 export class UserController {
   async create(request: FastifyRequest<{ Body: createUser }>, reply: FastifyReply) {
-    const { confirmPassword, email, password, username } = request.body;
+    const { confirm: confirmPassword, email, password, username } = request.body;
 
     if (confirmPassword !== password) {
       return reply.status(status.UNPROCESSABLE_ENTITY).send({ message: status[422] });
     }
 
-    await User.create({
+    const hashedPassword = await hash(password, 10);
+
+    const user = new User({
       email,
-      password,
+      password: hashedPassword,
       username,
     });
 
+    await user.save();
+
     reply.status(status.CREATED).send({
       message: status[201],
+      data: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
     });
   }
 }
