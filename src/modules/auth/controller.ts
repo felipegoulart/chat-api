437ad06c543552be343534d8f1ac2a628f6437e3
Luis Felipe Goulart <luisfelipegoulart94@gmail.com>
2025-10-03 15:21:34 -0300

    feat(auth): add logic to create user

--- src/modules/auth/controller.ts
+++ src/modules/auth/controller.ts
@@ -1,8 +1,40 @@
+import { hash } from "bcryptjs";
 import type { FastifyReply, FastifyRequest } from "fastify";
 import status from "http-status";
+import z from "zod";
+import { User } from "../user/index.js";
 
+export const createUserBodySchema = z
+  .object({
+    nickname: z.string().min(3).max(36),
+    email: z.email(),
+    password: z
+      .string()
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        "Your password must have at least 8 characters, one uppercase, one lowercase, one number and one special character",
+      ),
+    confirm: z.string(),
+  })
+  .refine((data) => data.confirm === data.password, {
+    error: "Passwords don't match",
+    path: ["confirm"],
+  });
+
+export type CreateUser = z.infer<typeof createUserBodySchema>;
 export class AuthController {
-  public register(request: FastifyRequest, reply: FastifyReply) {
+  public async register(request: FastifyRequest<{ Body: CreateUser }>, reply: FastifyReply) {
+    const { nickname, password, email } = request.body;
+
+    const result = await User.findOne({ email });
+    if (result) {
+      return reply.status(status.CONFLICT).send({ message: "User already exists" });
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    await User.create({ nickname, password: hashedPassword, email });
+
     return reply.status(status.CREATED).send({ message: status[201] });
   }
 }
--- src/modules/auth/routes.ts
+++ src/modules/auth/routes.ts
@@ -1,5 +1,5 @@
 import type { FastifyInstance } from "fastify";
-import { AuthController } from "./controller.js";
+import { AuthController, createUserBodySchema } from "./controller.js";
 
 const authController = new AuthController();
 
@@ -7,6 +7,9 @@ export const authRoutes = (app: FastifyInstance) => {
   app.route({
     method: "POST",
     url: "/register",
+    schema: {
+      body: createUserBodySchema,
+    },
     handler: authController.register.bind(authController),
   });
 };
