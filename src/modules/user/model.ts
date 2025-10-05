0116d118c38216c828f05d9bf09e2bfab953eb65
Luis Felipe Goulart <luisfelipegoulart94@gmail.com>
2025-10-05 16:52:02 -0300

    feat(user): add fields to verification logic

--- src/modules/user/model.ts
+++ src/modules/user/model.ts
@@ -6,6 +6,12 @@ export interface IUser {
   email: string;
   password: string;
   rooms: Types.ObjectId[];
+  verified: {
+    isVerified: boolean;
+    token: string | null;
+    tokenCreatedAt: Date | null;
+    verifiedAt: Date;
+  };
   createdAt: Date;
   updatedAt: Date;
 }
@@ -16,6 +22,12 @@ const userSchema = new Schema<IUser>(
     email: { type: String, required: true, unique: true, index: true },
     password: { type: String, required: true },
     rooms: [{ type: Schema.Types.ObjectId, ref: "Room" }],
+    verified: {
+      isVerified: { type: Boolean, default: false },
+      token: { type: String, default: null },
+      tokenCreatedAt: { type: Date, default: null },
+      verifiedAt: { type: Date, default: null },
+    },
   },
   { timestamps: true },
 );
