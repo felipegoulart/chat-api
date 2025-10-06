029719c3c3a22529e618f8596e3fb1ea9fe94cd6
Luis Felipe Goulart <luisfelipegoulart94@gmail.com>
2025-10-06 16:20:52 -0300

    feat(auth): set expiration time to token

--- src/modules/auth/controller.ts
+++ src/modules/auth/controller.ts
@@ -43,11 +43,12 @@ export class AuthController {
     }
 
     const refreshToken = await reply.jwtSign({ sub: user._id });
-    const accessToken = await reply.jwtSign({ sub: user._id }, { expiresIn: "15m" });
+    const accessToken = await reply.jwtSign({ sub: user._id }, { expiresIn: "7d" });
 
     reply.setCookie("refreshToken", refreshToken, {
       path: "/",
       httpOnly: true,
+      sameSite: "strict",
       expires: dayjs().add(7, "days").toDate(),
     });
 
@@ -135,7 +136,7 @@ export class AuthController {
     redis.set(currentRefreshToken, "revoked");
     redis.expire(currentRefreshToken, 60 * 60 * 24 * 7);
 
-    const newRefreshToken = await reply.jwtSign({ sub: request.user });
+    const newRefreshToken = await reply.jwtSign({ sub: request.user }, { expiresIn: "7d" });
     return reply
       .setCookie("refreshToken", newRefreshToken, {
         path: "/",
