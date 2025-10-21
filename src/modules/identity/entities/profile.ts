f5cd145eaf322a97381b98d3360ea89fffe113eb
Luis Felipe Goulart <luisfelipegoulart94@gmail.com>
2025-10-21 13:55:29 -0300

    feat(user): create class to user profile

---
+++ src/modules/identity/entities/profile.ts
@@ -0,0 +1,48 @@
+import z from "zod";
+
+const profileSchema = z.object({
+  nickname: z.string().min(3).max(32),
+  about: z.string().max(128).optional(),
+  avatarUrl: z.url().optional(),
+});
+
+type ProfileType = z.infer<typeof profileSchema>;
+
+export class Profile {
+  private constructor(
+    private nickname: string,
+    private about?: string,
+    private avatarUrl?: string,
+  ) {}
+
+  static create(value: ProfileType): Profile {
+    const profile = profileSchema.parse(value);
+    return new Profile(profile.nickname, profile.about, profile.avatarUrl);
+  }
+
+  public updateNickname(value: string): void {
+    const { nickname } = profileSchema.pick({ nickname: true }).parse(value);
+
+    this.nickname = nickname;
+  }
+
+  public updateAbout(value?: string): void {
+    const { about } = profileSchema.pick({ about: true }).parse(value);
+
+    this.about = about;
+  }
+
+  public updateAvatarUrl(value?: string): void {
+    const { avatarUrl } = profileSchema.pick({ avatarUrl: true }).parse(value);
+
+    this.avatarUrl = avatarUrl;
+  }
+
+  public toJSON(): ProfileType {
+    return {
+      nickname: this.nickname,
+      about: this.about,
+      avatarUrl: this.avatarUrl,
+    };
+  }
+}
