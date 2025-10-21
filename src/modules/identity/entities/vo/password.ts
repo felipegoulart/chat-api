b0ecd2162b29f5a446b5d9b92512e9baefe32fe2
Luis Felipe Goulart <luisfelipegoulart94@gmail.com>
2025-10-21 13:54:25 -0300

    feat(password): create value object to password

---
+++ src/modules/identity/entities/vo/password.ts
@@ -0,0 +1,37 @@
+import { compare, hash } from "bcryptjs";
+import z from "zod";
+
+export const passwordSchema = z
+  .string()
+  .regex(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,32}$/,
+    "Your password must have at least 8 at 32 characters, one uppercase, one lowercase, one number and one special character",
+  );
+
+export class Password {
+  readonly value: string;
+
+  private constructor(value: string) {
+    this.value = z.hash("sha256").parse(value);
+  }
+
+  static async create(value: string) {
+    const password = passwordSchema.parse(value);
+
+    const hashedPassword = await hash(password, 10);
+
+    return new Password(hashedPassword);
+  }
+
+  static restore(value: string) {
+    return new Password(value);
+  }
+
+  public async compare(password: string): Promise<boolean> {
+    return await compare(password, this.value);
+  }
+
+  public toString() {
+    return this.value.toString();
+  }
+}
