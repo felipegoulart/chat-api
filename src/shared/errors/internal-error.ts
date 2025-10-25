59b3ed1b0850e93ac5799c6c872afa315ada0a79
Luis Felipe Goulart <luisfelipegoulart94@gmail.com>
2025-10-25 20:22:39 -0300

    feat(InternalError): add codeAsString property to allow use error message

--- src/shared/errors/internal-error.ts
+++ src/shared/errors/internal-error.ts
@@ -1,10 +1,15 @@
+import status from "statuses";
+
 export class InternalError extends Error {
+  public readonly codeAsString: string;
+
   constructor(
     public readonly message: string,
     public readonly code: number = 500,
     public readonly description?: string,
   ) {
     super(message);
+    this.codeAsString = status(code);
     this.name = this.constructor.name;
     Error.captureStackTrace(this, this.constructor);
   }
