6097bf7046cb447d8cc17b3b8e419f3f4616e94d
Luis Felipe Goulart <luisfelipegoulart94@gmail.com>
2025-10-25 00:55:09 -0300

    feat(auth): create auth repository

---
+++ src/modules/identity/application/services/auth.service.ts
@@ -0,0 +1,47 @@
+import { User } from "../../entities/user.js";
+import type { UserRepository } from "../../infrastructure/repository.js";
+
+type RegisterUserInput = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+  nickname: string;
+  about?: string;
+  avatarUrl?: string;
+};
+
+export class AuthService {
+  constructor(private readonly repository: UserRepository) {}
+
+  public async register({
+    nickname,
+    confirmPassword,
+    email,
+    password,
+    about,
+    avatarUrl,
+  }: RegisterUserInput): Promise<User> {
+    if (password !== confirmPassword) {
+      throw new Error("Passwords do not match");
+    }
+
+    const result = await this.repository.findByEmail(email);
+    if (result) {
+      throw new Error("User already exists");
+    }
+
+    const user = await User.create({
+      email,
+      password,
+      profile: {
+        nickname,
+        about,
+        avatarUrl,
+      },
+    });
+
+    user.setVerificationToken();
+
+    return await this.repository.create(user);
+  }
+}
